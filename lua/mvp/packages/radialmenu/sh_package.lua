7c9ddfabb3d83e1a3330e9eecf1b650bb8da50fa
ggdropkal <44447172+Kotyarishka@users.noreply.github.com>
2024-02-16 16:52:27 +0200

    Initial

---
+++ lua/mvp/packages/radialmenu/sh_package.lua
@@ -0,0 +1,8 @@
+local P = mvp.meta.package:New()
+
+P:SetName("Radial Menus")
+P:SetVersion("1.0.0")
+P:SetDescription("Allows you to create radial menus in Garry's Mod")
+P:SetAuthor("Kot")
+
+mvp.package.Register(P)
