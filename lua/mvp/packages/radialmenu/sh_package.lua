2535f9f7f8a30e22eeaee1d007395e0bdc0828ce
ggdropkal <44447172+Kotyarishka@users.noreply.github.com>
2024-04-13 02:20:33 +0300

    bump version

--- lua/mvp/packages/radialmenu/sh_package.lua
+++ lua/mvp/packages/radialmenu/sh_package.lua
@@ -2,7 +2,7 @@ local P = mvp.meta.package:New()
 
 P:SetIcon(Material("mvp/radialmenu/package_icon.png", "smooth"))
 P:SetName("Radial Menus")
-P:SetVersion("1.0.0")
+P:SetVersion("1.0.2")
 P:SetDescription("Enables developers to create radial menus for their needs.")
 P:SetAuthor("Kot")
 
